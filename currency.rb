17b95645dd3ea113b692519f105f18d475531bc4
Lori Burleson <lori.burleson@gmail.com>
2016-09-19 13:41:46 -0400

    Will perform add_amounts function correctly - aka only when currency code is the same. Otherwise returns false.

--- currency.rb
+++ currency.rb
@@ -2,8 +2,17 @@ class Currency
 attr_reader :amount, :currency_code
 # should things be passed in here or no? Not sure about when to do that or not.
   def initialize(amount, currency_code)
-    @amount = amount
+    @amount = amount.to_f
     @currency_code = currency_code
+  end
+
+  def add_amounts(account_one, account_two)
+    if account_one.currency_code == account_two.currency_code
+      added_amounts = account_one.amount + account_two.amount
+      return added_amounts
+    else
+      return false
+    end
   end
 
 end
